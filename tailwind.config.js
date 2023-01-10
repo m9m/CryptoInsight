f1bd7ec5506790bd81b9e5110bb44bc7266686c9
m9m <95250150+m9m@users.noreply.github.com>
2023-01-10 16:15:58 -0600

    added gradient colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.{html,js,ejs}","./views/*.ejs"],
   theme: {
     extend: {},
+    colors: {
+      blue: colors.blue,
+      purple: colors.purple
+    }
   },
   plugins: [],
 }
