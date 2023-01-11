015342e4a877c5d6ea6773a6d0b51b2e4e140bf8
m9m <>
2023-01-10 23:36:27 -0600

    Changed files to support redirect index > home

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// redirect to the default home page
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.redirect('/home');
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('home');
 });
 
 module.exports = router;
